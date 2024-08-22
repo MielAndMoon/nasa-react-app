41263bd41acc93cafe7e7f5d141baf87ece014fd
MielAndMoon <dmestasa@gmail.com>
2024-08-21 22:37:47 -0500

    refactor(main): delete the container div

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,9 +4,6 @@ interface Props {
 
 export const Main = (props: Props) => {
     return (
-        <div>
-            <img src="mars.png" alt="mar demo picture" />
-        </div>
+        <img src="mars.png" alt="mar demo picture" />
     )
-
 }
