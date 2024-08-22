87c17e812bf861bc6e43706907b2226f4c2d9648
MielAndMoon <dmestasa@gmail.com>
2024-08-21 22:40:11 -0500

    feat(side-bar): add the side-bar section

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,15 @@
+interface Props {
+
+}
+
+export const SideBar = (props: Props) => {
+    return (
+        <div>
+            <h2>The brutan Marcial landspace</h2>
+            <div>
+                <p>Description</p>
+                <p>This is a descripton of the image of Marcial planet</p>
+            </div>
+        </div>
+    )
+}
