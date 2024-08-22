53027a68d12f6c0d40597eaa88dcbc192aa5ce22
MielAndMoon <dmestasa@gmail.com>
2024-08-22 11:39:35 -0500

    style(side-bar): add styles to the side-bar section

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,11 +4,17 @@ interface Props {
 
 export const SideBar = (props: Props) => {
     return (
-        <div>
-            <h2>The brutan Marcial landspace</h2>
-            <div>
-                <p>Description</p>
-                <p>This is a descripton of the image of Marcial planet</p>
+        <div className="sidebar">
+            <div className="bg-overlay"></div>
+            <div className="sidebar-contents">
+                <h2>The brutan Marcial landspace</h2>
+                <div>
+                    <p>Description</p>
+                    <p>This is a descripton of the image of Marcial planet</p>
+                </div>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 12h16m-7-7l7 7l-7 7" /></svg>
+                </button>
             </div>
         </div>
     )
