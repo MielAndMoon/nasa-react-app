6f831bdcdcd7cae7238c10b2c4c71bb0ae065bf4
MielAndMoon <dmestasa@gmail.com>
2024-08-22 13:30:41 -0500

    feat(footer): add content from the nasa api

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,20 @@
 interface Props {
+    data: {
+        title: string,
+        copyright: string
+    },
     handleToggleModal: () => void
 }
 
 export const Footer = (
-    { handleToggleModal }: Props
+    { data, handleToggleModal }: Props
 ) => {
     return (
         <footer>
             <div className="bg-gradient"></div>
             <div>
-                <h2>The brutal Marcial Landscape</h2>
-                <h1>APOD PROJECT</h1>
+                <h2>{data.title}</h2>
+                <h1>{data.copyright}</h1>
             </div>
             <button onClick={() => handleToggleModal()}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11s11-4.925 11-11S18.075 1 12 1m-.5 5a1 1 0 1 0 0 2h.5a1 1 0 1 0 0-2zM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1z" clipRule="evenodd" /></svg>
